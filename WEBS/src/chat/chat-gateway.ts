d9187f725ad988564dc305c83b705f3ccf4663f6
Ashvatth Joshi <codingash27@gmail.com>
2025-05-24 00:35:25 +0530

    basic chat app

--- WEBS/src/chat/chat-gateway.ts
+++ WEBS/src/chat/chat-gateway.ts
@@ -1,13 +1,51 @@
 import {
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
 
+import { Server, Socket } from 'socket.io';
+
 @WebSocketGateway(3002, {})
-export class ChatGateWay {
+export class ChatGateWay implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer() server: Server;
+
+  handleConnection(client: Socket) {
+    console.log('new user connected', client.id);
+
+    this.server.emit('user-joined', {
+      message: `new user joined the chat: ${client.id}`,
+    });
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log('user disconnected', client.id);
+
+    this.server.emit('user-left', {
+      message: `user left the chat: ${client.id}`,
+    });
+  }
+
+  //   @SubscribeMessage('newMessage')
+  //   handleNewMessage(client: Socket, message: any) {
+  //     console.log(message);
+
+  //     client.emit('reply', 'this is a reply from the server');
+
+  //     this.server.emit('reply', 'broadcasting to all clients from the server');
+  //   }
+
   @SubscribeMessage('newMessage')
-  handleNewMessage(@MessageBody() message: any) {
-    console.log(message);
+  handleNewMessage(@MessageBody() message: string) {
+    this.server.emit('message', message); // broadcast to all clients
   }
 }
+
+// socket.on
+
+// io.emit - used to emit to all clients
+
+// socket.emit - used to emit to a specific client or one single client
